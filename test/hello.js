cf91ccb2d0648e3516c32c1030e5c0b3462231e9
kevin24lg <kevinhaller24@gmail.com>
2016-12-06 22:43:45 -0800

    test

---
+++ test/hello.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env nodejs
+
+var http = require("http");
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Hello World, bitch");
+}).listen(8080, "localhost");
+
+console.log("Server running at http://localhost:8080/");
+
